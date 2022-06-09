6b782eccf529163565740709a48f1aede7250fdb
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-06-09 21:16:02 +0530

    feature: added token parsing repl

--- src/repl.rs
+++ src/repl.rs
@@ -1,7 +1,30 @@
-use crate::lexer::Lexer;
+use crate::{lexer::Lexer, token, token::Token};
+use std::io::{self, Write};
 
 const PROMPT: &str = ">> ";
 
 pub fn start() {
-    
+    loop {
+        let input = get_input();
+        let mut lexer = Lexer::new(input);
+        let mut token: token::Token;
+        loop {
+            token = lexer.next_token();
+            if token == Token::Eof {
+                break;
+            } else {
+                println!("{}", token);
+            }
+        }
+    }
+}
+
+fn get_input() -> String {
+    let mut stdout = io::stdout();
+    let stdin = io::stdin();
+    let mut input = String::new();
+    print!("{}", PROMPT);
+    stdout.flush().expect("Failed to flush");
+    stdin.read_line(&mut input).expect("Failed to read line");
+    input
 }
