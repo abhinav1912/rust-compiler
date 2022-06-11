c91f73d578e81fa20e645b3cd5c17add0b97f9cd
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-06-11 22:29:43 +0530

    feat: added Debug and PartialEq traits for Statement and Expression

--- src/ast.rs
+++ src/ast.rs
@@ -1,10 +1,12 @@
 use std::{fmt, vec};
 
+#[derive(Debug, PartialEq)]
 pub enum Expression {
     Identifier(String),
     IntegerLiteral(i64)
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Statement {
     Let(String, Expression),
     Expression(Expression)
