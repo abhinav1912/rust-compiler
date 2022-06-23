2057417b22a7736e8e3736dfe166a87dccfe1e15
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-06-23 21:20:01 +0530

    wip: added Prefix, Infix and BlockStatement enums

--- src/ast.rs
+++ src/ast.rs
@@ -3,7 +3,36 @@ use std::{fmt, vec};
 #[derive(Debug, PartialEq)]
 pub enum Expression {
     Identifier(String),
-    IntegerLiteral(i64)
+    IntegerLiteral(i64),
+    FloatLiteral(f64),
+    StringLiteral(String),
+    Boolean(bool),
+    Array(Vec<Expression>),
+    Hash(Vec<(Expression, Expression)>),
+    Index(Box<Expression>, Box<Expression>),
+    Prefix(Prefix, Box<Expression>),
+    Infix(Infix, Box<Expression>, Box<Expression>),
+    If(Box<Expression>, BlockStatement, Option<BlockStatement>),
+    FunctionLiteral(Vec<String>, BlockStatement),
+    Call(Box<Expression>, Vec<Expression>),
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Prefix {
+    Bang,
+    Minus,
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub enum Infix {
+    Eq,
+    NotEq,
+    Lt,
+    Gt,
+    Plus,
+    Minus,
+    Asterisk,
+    Slash,
 }
 
 #[derive(Debug, PartialEq)]
@@ -11,6 +40,11 @@ pub enum Statement {
     Let(String, Expression),
     Return(Option<Expression>),
     Expression(Expression)
+}
+
+#[derive(Debug, PartialEq, Clone)]
+pub struct BlockStatement {
+    pub statements: Vec<Statement>,
 }
 
 pub struct Program {
