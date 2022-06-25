501ae70aa1b1ceeb6c3380642b3cc93b5642de32
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-06-25 18:59:03 +0530

    feat: added int and bool evaluation

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -1,17 +1,24 @@
 use crate::{ast::{Program, Statement, Expression}, object::{Object, EvalError, EvalResult}};
 
-pub fn eval(program: &Program) -> Object {
+pub fn eval(program: &Program) -> EvalResult {
     let mut result = Object::Null;
     for statement in &program.statements {
-        // result = 
+        result = eval_statement(statement)?;
     }
-    result
+    Ok(result)
 }
 
 fn eval_statement(statement: &Statement) -> EvalResult {
-    return Ok(Object::Null);
+    match statement {
+        Statement::Expression(expression) => eval_expression(expression),
+        _ => Ok(Object::Null),
+    }
 }
 
-fn eval_expression(expression: Expression) -> EvalResult {
-    return Ok(Object::Null);
+fn eval_expression(expression: &Expression) -> EvalResult {
+    match expression {
+        Expression::IntegerLiteral(value) => Ok(Object::Integer(*value)),
+        Expression::Boolean(value) => Ok(Object::Boolean(*value)),
+        _ => Ok(Object::Null)
+    }
 }
