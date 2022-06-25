0559695e2cb07567977228d5e3ec8577819ea414
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-06-25 18:45:28 +0530

    wip: exposed EvalError enum

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,6 +1,13 @@
+pub type EvalResult = Result<Object, EvalError>;
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum Object {
     Integer(i64),
     Boolean(bool),
     Null
+}
+
+#[derive(Debug)]
+pub enum EvalError {
+
 }
