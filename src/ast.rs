b7abb7a93c977a0afd596887c34dbd93e42e8ba7
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-06-25 11:36:17 +0530

    feat: implemented Display for all enums

--- src/ast.rs
+++ src/ast.rs
@@ -50,3 +50,102 @@ pub struct BlockStatement {
 pub struct Program {
     pub statements: Vec<Statement>
 }
+
+impl fmt::Display for Expression {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Expression::Identifier(ident) => write!(f, "{}", ident),
+            Expression::IntegerLiteral(value) => write!(f, "{}", value),
+            Expression::FloatLiteral(value) => write!(f, "{}", value),
+            // TODO: Escape `"`
+            Expression::StringLiteral(s) => write!(f, "\"{}\"", s),
+            Expression::Boolean(value) => write!(f, "{}", value),
+            Expression::Array(values) => write!(f, "[{}]", comma_separated(values)),
+            Expression::Hash(pairs) => {
+                let items = pairs
+                    .iter()
+                    .map(|(k, v)| format!("{}: {}", k, v))
+                    .collect::<Vec<String>>();
+                write!(f, "{{{}}}", items.join(", "))
+            }
+            Expression::Index(left, index) => write!(f, "({}[{}])", left, index),
+            Expression::Prefix(operator, exp) => write!(f, "({}{})", operator, exp),
+            Expression::Infix(operator, left, right) => {
+                write!(f, "({} {} {})", left, operator, right)
+            }
+            Expression::If(condition, consequence, alternative) => {
+                write!(f, "if {} {}", condition, consequence)?;
+                if let Some(alt) = alternative {
+                    write!(f, " else {}", alt)?;
+                }
+                Ok(())
+            }
+            Expression::FunctionLiteral(parameters, body) => {
+                write!(f, "fn({}) {}", parameters.join(", "), body)
+            }
+            Expression::Call(function, arguments) => {
+                write!(f, "{}({})", function, comma_separated(arguments))
+            }
+        }
+    }
+}
+
+impl fmt::Display for Infix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Infix::Eq => write!(f, "=="),
+            Infix::NotEq => write!(f, "!="),
+            Infix::Lt => write!(f, "<"),
+            Infix::Gt => write!(f, ">"),
+            Infix::Plus => write!(f, "+"),
+            Infix::Minus => write!(f, "-"),
+            Infix::Asterisk => write!(f, "*"),
+            Infix::Slash => write!(f, "/"),
+        }
+    }
+}
+
+impl fmt::Display for Prefix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Prefix::Bang => write!(f, "!"),
+            Prefix::Minus => write!(f, "-"),
+        }
+    }
+}
+
+impl fmt::Display for BlockStatement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for stmt in &self.statements {
+            write!(f, "{{ {} }}", stmt)?;
+        }
+        Ok(())
+    }
+}
+
+impl fmt::Display for Statement {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Statement::Let(ident, value) => write!(f, "let {} = {};", ident, value),
+            Statement::Return(None) => write!(f, "return;"),
+            Statement::Return(Some(exp)) => write!(f, "return {};", exp),
+            Statement::Expression(exp) => write!(f, "{};", exp),
+        }
+    }
+}
+
+impl fmt::Display for Program {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        for statement in &self.statements {
+            write!(f, "{}", statement)?;
+        }
+        Ok(())
+    }
+}
+
+fn comma_separated(exps: &[Expression]) -> String {
+    exps.iter()
+        .map(|a| a.to_string())
+        .collect::<Vec<String>>()
+        .join(", ")
+}
