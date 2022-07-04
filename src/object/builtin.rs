58af748f937fdb64bcc8c5f31e9be9f297a39c46
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-07-04 09:23:18 +0530

    feat: added first/last/push for arrays

--- src/object/builtin.rs
+++ src/object/builtin.rs
@@ -15,7 +15,11 @@ macro_rules! builtin {
 }
 
 pub const BUILTINS: &[Builtin] = &[
-    builtin!(len)
+    builtin!(len),
+    builtin!(first),
+    builtin!(last),
+    builtin!(rest),
+    builtin!(push)
 ];
 
 fn len(arguments: Vec<Object>) -> EvalResult {
@@ -24,6 +28,58 @@ fn len(arguments: Vec<Object>) -> EvalResult {
         Object::String(value) => Ok(Object::Integer(value.len() as i64)),
         Object::Array(array) => Ok(Object::Integer(array.len() as i64)),
         _ => Err(EvalError::UnsupportedArguments("len".to_string(), arguments))
+    }
+}
+
+fn first(arguments: Vec<Object>) -> EvalResult {
+    assert_argument_count(1, &arguments)?;
+    match &arguments[0] {
+        Object::Array(array) => {
+            Ok(match array.first() {
+                Some(element) => element.clone(),
+                None => Object::Null
+            })
+        },
+        _ => Err(EvalError::UnsupportedArguments("first".to_string(), arguments))
+    }
+}
+
+fn last(arguments: Vec<Object>) -> EvalResult {
+    assert_argument_count(1, &arguments)?;
+    match &arguments[0] {
+        Object::Array(array) => {
+            Ok(match array.last() {
+                Some(element) => element.clone(),
+                None => Object::Null
+            })
+        },
+        _ => Err(EvalError::UnsupportedArguments("last".to_string(), arguments))
+    }
+}
+
+fn rest(arguments: Vec<Object>) -> EvalResult {
+    assert_argument_count(1, &arguments)?;
+    match &arguments[0] {
+        Object::Array(array) => {
+            if array.len() < 2 {
+                return Ok(Object::Null)
+            }
+            let slice = array[1..].to_vec();
+            return Ok(Object::Array(slice))
+        },
+        _ => Err(EvalError::UnsupportedArguments("rest".to_string(), arguments))
+    }
+}
+
+fn push(arguments: Vec<Object>) -> EvalResult {
+    assert_argument_count(2, &arguments)?;
+    match &arguments[0] {
+        Object::Array(array) => {
+            let mut values = array.clone();
+            values.push(arguments[1].clone());
+            Ok(Object::Array(values))
+        },
+        _ => Err(EvalError::UnsupportedArguments("push".to_string(), arguments))
     }
 }
 
