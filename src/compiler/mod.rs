127177b0391b915b4f8fa59a4fa8fe05997d4393
abhinav1912 <abhinavmathur1912@gmail.com>
2022-07-14 03:43:13 +0000

    wip: initial methods for Compiler

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -1,8 +1,30 @@
 use std::{cell::RefCell, rc::Rc};
 
-use crate::{code::Instructions, object::Object};
+use crate::{ast::Program, code::Instructions, object::Object};
 
 pub struct Compiler {
     pub constants: Rc<RefCell<Vec<Object>>>,
     pub instructions: Rc<RefCell<Vec<Instructions>>>
+}
+
+pub struct ByteCode {
+    pub constants: Rc<RefCell<Vec<Object>>>,
+    pub instructions: Rc<RefCell<Vec<Instructions>>>
+}
+
+impl Compiler {
+    pub fn new() -> Self {
+        return Compiler {
+            constants: Rc::new(RefCell::new(vec![])),
+            instructions: Rc::new(RefCell::new(vec![]))
+        }
+    }
+
+    pub fn compile(mut self, program: &Program) {
+        return 
+    }
+
+    pub fn bytecode(self) -> ByteCode {
+        return ByteCode { constants: self.constants, instructions: self.instructions }
+    }
 }
