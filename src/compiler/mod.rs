873d78442175f9dc1601357cf67dee3bcf5ccd72
Abhinav Mathur <abhinavmathur1912@gmail.com>
2022-07-17 11:27:33 +0530

    fix: fixed the failing kids

--- src/compiler/mod.rs
+++ src/compiler/mod.rs
@@ -29,13 +29,54 @@ impl Compiler {
 
     pub fn compile_statement(&mut self, statement: &Statement) -> Result<(), CompileError> {
         match statement {
-            Statement::Expression(expression) => self.compile_expression(expression),
-            _ => Err(CompileError::CompilingNotImplemented)
+            Statement::Expression(expression) => {
+                self.compile_expression(expression)?;
+                self.emit(OpCode::Pop);
+            },
+            _ => return Err(CompileError::CompilingNotImplemented)
         }
+        Ok(())
     }
 
     pub fn compile_expression(&mut self, expression: &Expression) -> Result<(), CompileError> {
         match expression {
+            Expression::Infix(infix, left, right) => {
+                match infix {
+                    Infix::Lt => {
+                        // Convert `a < b` to `a > b` to keep the instruction set smaller.
+                        // TODO: This is an issue if expressions have side effects though.
+                        self.compile_expression(right)?;
+                        self.compile_expression(left)?;
+                    }
+                    _ => {
+                        self.compile_expression(left)?;
+                        self.compile_expression(right)?;
+                    }
+                }
+                match infix {
+                    Infix::Plus => {
+                        self.emit(OpCode::Add);
+                    }
+                    Infix::Minus => {
+                        self.emit(OpCode::Sub);
+                    }
+                    Infix::Asterisk => {
+                        self.emit(OpCode::Mul);
+                    }
+                    Infix::Slash => {
+                        self.emit(OpCode::Div);
+                    }
+                    Infix::Eq => {
+                        self.emit(OpCode::Equal);
+                    }
+                    Infix::NotEq => {
+                        self.emit(OpCode::NotEqual);
+                    }
+                    Infix::Gt | Infix::Lt => {
+                        self.emit(OpCode::GreaterThan);
+                    }
+                }
+            }
             Expression::IntegerLiteral(value) => {
                 let constant = Constant::Integer(*value);
                 let const_index = self.add_constant(constant)?;
@@ -89,6 +130,91 @@ impl fmt::Display for CompileError {
             CompileError::TooManyLocals => write!(f, "too many local bindings in a function"),
             CompileError::TooManyFrees => write!(f, "too many free bindings in a function"),
             CompileError::CompilingNotImplemented => write!(f, "Not implemeneted"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Compiler;
+    use crate::ast::Program;
+    use crate::code::{
+        make, make_u16, make_u16_u8, make_u8, Constant, print_instructions,
+        Instructions, OpCode,
+    };
+    use crate::lexer::Lexer;
+    use crate::parser::Parser;
+
+    #[test]
+    fn compile() {
+        test_compile(vec![
+            (
+                "1 + 2",
+                vec![Constant::Integer(1), Constant::Integer(2)],
+                vec![
+                    make_u16(OpCode::Constant, 0),
+                    make_u16(OpCode::Constant, 1),
+                    make(OpCode::Add),
+                ],
+            ),
+        ]);
+    }
+
+    fn test_compile(tests: Vec<(&str, Vec<Constant>, Vec<Instructions>)>) {
+        for (input, expected_constants, expected_instructions) in tests {
+            let program = parse(input);
+
+            let compiler = Compiler::new();
+            let bytecode = match compiler.compile(&program) {
+                Ok(bytecode) => bytecode,
+                Err(error) => panic!("failed to compile input `{}`: {}", input, error),
+            };
+
+            // Compare instructions.
+            assert_eq!(
+                print_instructions(&bytecode.instructions),
+                print_instructions(&expected_instructions.concat()),
+                "\nfor `{}`",
+                input
+            );
+
+            if bytecode.constants.len() != expected_constants.len() {
+                assert_eq!(bytecode.constants, expected_constants, "\nfor {}", input);
+            }
+
+            // Pretty-print instructions in error messages
+            let pairs = bytecode.constants.iter().zip(expected_constants.iter());
+            for (i, (constant, expected_constant)) in pairs.enumerate() {
+                match (constant, expected_constant) {
+                    _ => {
+                        assert_eq!(
+                            constant, expected_constant,
+                            "\nconstant (index {}) for {}",
+                            i, input
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    fn parse(input: &str) -> Program {
+        let lexer = Lexer::new(input.to_owned());
+        let mut parser = Parser::new_parser(lexer);
+
+        let program = parser.parse_program();
+        check_parser_errors(&parser);
+        program
+    }
+
+    fn check_parser_errors(parser: &Parser) {
+        let errors = parser.errors();
+        if errors.len() > 0 {
+            panic!(
+                "for input '{}', got parser errors: {:?}",
+                parser.input(),
+                errors
+            );
         }
     }
 }
--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,6 +1,6 @@
-use std::{rc::Rc};
+use std::{rc::Rc, fmt};
 
-use crate::{object::Object, code::{Instructions, Constant, OpCode, self}, compiler::{ByteCode, CompileError}};
+use crate::{object::{Object, EvalError}, code::{Instructions, Constant, OpCode, self}, compiler::{ByteCode, CompileError}};
 
 pub const STACK_SIZE: usize = 2048;
 pub const NULL: Object = Object::Null;
@@ -13,6 +13,31 @@ pub struct Vm {
     pointer: usize
 }
 
+#[derive(Debug)]
+pub enum VmError {
+    UnknownOpCode(u8),
+    InvalidConstIndex(usize, usize),
+    StackOverflow,
+    StackEmpty,
+    NotFunction(Constant),
+    Eval(EvalError),
+}
+
+impl fmt::Display for VmError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            VmError::UnknownOpCode(op_code) => write!(f, "unknown op code: {}", op_code),
+            VmError::InvalidConstIndex(given, length) => {
+                write!(f, "invalid const index: {} / {}", given, length)
+            }
+            VmError::StackOverflow => write!(f, "stack overflow"),
+            VmError::StackEmpty => write!(f, "stack empty"),
+            VmError::NotFunction(con) => write!(f, "not a function: {}", con.type_name()),
+            VmError::Eval(eval_error) => write!(f, "{}", eval_error),
+        }
+    }
+}
+
 impl Vm {
     pub fn new(bytecode: ByteCode) -> Self {
         let mut stack = Vec::with_capacity(STACK_SIZE);
@@ -22,39 +47,103 @@ impl Vm {
         return Vm {
             constants: bytecode.constants,
             instructions: bytecode.instructions,
-            stack: vec![],
+            stack,
             pointer: 0
         }
     }
 
-    pub fn run(&mut self) -> Result<(), CompileError> {
-        for pointer in 0.. self.instructions.len() {
+    pub fn run(mut self) -> Result<Rc<Object>, VmError> {
+        let mut pointer = 0;
+        while pointer < self.instructions.len() {
             let op = OpCode::from_byte(self.instructions[pointer]);
+            println!("fk {:?}, {}", self.instructions, pointer);
             match op {
                 Some(OpCode::Constant) => {
-                    let const_index = code::read_uint16(&self.instructions, self.pointer+1) as usize;
-                    self.pointer += 2;
+                    let const_index = code::read_uint16(&self.instructions, pointer+1) as usize;
+                    pointer += 2;
 
                     let len = self.constants.len();
+                    println!("rrr {}, {}", const_index, len);
                     if const_index < len {
                         let constant = Object::from_constant(&self.constants[const_index]);
                         self.push(Rc::new(constant))?;
                     } else {
-                        return Err(CompileError::TooManyConstants) // TODO: Fix error
+                        return Err(VmError::InvalidConstIndex(const_index, len));
                     }
+                },
+                Some(OpCode::Pop) => {
+                    self.pop()?;
                 }, 
-                _ => return Err(CompileError::CompilingNotImplemented)
+                _ => return Err(VmError::UnknownOpCode(self.instructions[pointer]))
             }
+            pointer += 1;
         }
-        Ok(())
+        self.stack
+            .get(self.pointer)
+            .map(|o| Rc::clone(o))
+            .ok_or(VmError::StackEmpty)
     }
 
-    fn push(&mut self, obj: Rc<Object>) -> Result<(), CompileError> {
+    fn push(&mut self, obj: Rc<Object>) -> Result<(), VmError> {
         if self.pointer >= STACK_SIZE {
-            return Err(CompileError::TooManyConstants)
+            return Err(VmError::StackOverflow);
         }
         self.stack[self.pointer] = obj;
         self.pointer += 1;
         Ok(())
+    }
+
+    fn pop(&mut self) -> Result<Rc<Object>, VmError> {
+        let popped = self.stack.get(self.pointer - 1);
+        self.pointer -= 1;
+        popped.map(|o| Rc::clone(o)).ok_or(VmError::StackEmpty)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{lexer::Lexer, parser::Parser, compiler::Compiler};
+
+    use super::Vm;
+
+    #[test]
+    fn integer() {
+        expect_values(vec![
+            ("1", "1"),
+        ]);
+    }
+
+    fn expect_values(tests: Vec<(&str, &str)>) {
+        for (input, expected) in tests {
+            let vm = make_vm(input);
+            match vm.run() {
+                Ok(obj) => {
+                    assert_eq!(&obj.to_string(), expected, "for `{}`", input);
+                }
+                Err(err) => {
+                    panic!("error on vm for `{}`: {}", input, err);
+                }
+            }
+        }
+    }
+
+    fn make_vm(input: &str) -> Vm {
+        let lexer = Lexer::new(input.to_owned());
+        let mut parser = Parser::new_parser(lexer);
+        let program = parser.parse_program();
+        let errors = parser.errors;
+        if errors.len() > 0 {
+            panic!("for input '{}', got parser errors: {:?}", input, errors);
+        }
+
+        let compiler = Compiler::new();
+        let bytecode = match compiler.compile(&program) {
+            Ok(bytecode) => bytecode,
+            Err(err) => {
+                panic!("error on compile for `{}`: {}", input, err);
+            }
+        };
+        println!("ttt {:?}", bytecode.constants);
+        Vm::new(bytecode)
     }
 }
