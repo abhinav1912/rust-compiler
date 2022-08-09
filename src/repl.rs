a195c6050b405fcdbaf95e116af7d86cf558d88e
abhinav1912 <abhinavmathur1912@gmail.com>
2022-08-09 14:09:07 +0530

    feat: builtin execution

--- src/repl.rs
+++ src/repl.rs
@@ -1,58 +1,85 @@
 use crate::{lexer::Lexer, parser::{Parser, ParserError}, evaluator, object::environment::Environment, mode::Mode, compiler::{Compiler, symbol_table::SymbolTable}, vm::{Vm, self}};
-use std::{io::{self, Write}, rc::Rc, cell::RefCell};
+use std::{io::{self, Write}, rc::Rc, cell::RefCell, env};
 
 const PROMPT: &str = ">> ";
 
 pub fn start(mode: Mode) {
+    let username = env::var("LOGNAME").unwrap_or_else(|_| "anonymous".to_string());
+    println!(
+        "Hello {}! This is the 🐒 programming language in {}!",
+        username, mode
+    );
+    println!("Feel free to type in commands");
+
+    // For evaluator
     let env = Rc::new(RefCell::new(Environment::new()));
+
+    // For compiler and VM
+    // The compiler adds more constants and entries to the symbol table.
     let constants = Rc::new(RefCell::new(Vec::new()));
-    let symbol_table = Rc::new(RefCell::new(SymbolTable::new()));
+    let symbol_table = Rc::new(RefCell::new(SymbolTable::new_with_builtins()));
+    // The VM adds more globals.
     let globals = Rc::new(RefCell::new(vm::new_globals()));
+
     loop {
-        let input = get_input();
-        let lexer = Lexer::new(input);
-        let mut parser = Parser::new_parser(lexer);
+        let input = ask_input(">> ");
+
+        let mut parser = Parser::new_parser(Lexer::new(input));
+
         let program = parser.parse_program();
-        if !parser.errors.is_empty() {
-            print_parser_errors(parser.errors);
+        if !parser.errors().is_empty() {
+            println!("Woops! We ran into some monkey business here!");
+            println!(" parser errors:");
+            for error in parser.errors() {
+                println!("\t{:?}", error);
+            }
             continue;
         }
+
         match mode {
             Mode::Eval => match evaluator::eval(&program, Rc::clone(&env)) {
-                Ok(obj) => println!("{}", obj),
-                Err(err) => println!("{}", err)
+                Ok(obj) => {
+                    println!("{}", obj);
+                }
+                Err(err) => {
+                    println!("ERROR: {}", err);
+                }
             },
             Mode::Compile => {
-                let compiler = Compiler::new_with_state(Rc::clone(&symbol_table), Rc::clone(&constants));
+                let compiler =
+                    Compiler::new_with_state(Rc::clone(&symbol_table), Rc::clone(&constants));
                 let bytecode = match compiler.compile(&program) {
                     Ok(bytecode) => bytecode,
                     Err(err) => {
-                        println!("Compilation failed: {}", err);
+                        println!("Woops! Compilation failed: {}", err);
                         continue;
                     }
                 };
+
                 let vm = Vm::new_with_globals_store(bytecode, Rc::clone(&globals));
                 match vm.run() {
-                    Ok(result) => println!("{}", result),
-                    Err(err) => println!("{}", err)
+                    Ok(result) => {
+                        println!("{}", result);
+                    }
+                    Err(err) => {
+                        println!("Woops! Executing bytecode failed: {}", err);
+                    }
                 }
             }
         }
     }
 }
 
-fn print_parser_errors(errors: Vec<ParserError>) {
-    for error in errors {
-        println!("\t{:?}", error);
-    }
-}
-
-fn get_input() -> String {
+fn ask_input(prompt: &str) -> String {
     let mut stdout = io::stdout();
     let stdin = io::stdin();
     let mut input = String::new();
-    print!("{}", PROMPT);
-    stdout.flush().expect("Failed to flush");
-    stdin.read_line(&mut input).expect("Failed to read line");
+
+    print!("{}", prompt);
+    stdout.flush().expect("Failed to flush stdout");
+    stdin
+        .read_line(&mut input)
+        .expect("Failed to read line from stdin");
+
     input
 }
